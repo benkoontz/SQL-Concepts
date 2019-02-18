d9f44739022912f07f974e95b565b6b8a6fcf729
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-18 10:22:50 -0700

    Create ben.sql

---
+++ 30_Numeric Functions/ben.sql
@@ -0,0 +1,13 @@
+# Numeric functions
+SELECT CEIL(35.40); # prints 36
+SELECT FLOOR(35.40); # prints 35
+SELECT EXP(5); # e^5, e is about 2.7
+SELECT LOG(2); # natural log of 2
+SELECT LOG10(2); # Finds log base 10
+SELECT POW(2,7); # outputs 2^7 
+SELECT GREATEST(5,3,1,77,9,11); # takes list of numbers and returns highest number, prints 77
+SELECT LEAST(5,3,1,77,9,11); # returns smallest number, returns 1
+SELECT RADIANS(180); # converts degrees to radians
+SELECT SQRT(225); # prints 15
+SELECT TRUNCATE(225.2421412, 2); # prints 225.24
+SELECT RAND(); # creates a random number
