ce78136872b8a02d6e5704cbfbc31eef75b00048
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-18 10:45:46 -0700

    Create ben.sql

---
+++ 35_Length() & Insert() Functions/ben.sql
@@ -0,0 +1,6 @@
+# LENGTH() and INSERT() Functions
+
+SELECT LENGTH('Hello World'); # prints the total length occupied by memory
+SELECT CHAR_LENGTH('Hello World'); # counts number of characters
+SELECT INSERT('Hello Word',7,5,'World') # first arg is original string, 2nd is position of string you want replaced
+# 3rd arg is number of characters you want to replace in string, 4th is new string
