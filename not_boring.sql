f1dfe3c61c6cbb33b8548b49b9d61dc45ae39f48
benkoontz <36342241+benkoontz@users.noreply.github.com>
2020-09-20 12:37:59 -0700

    Create not_boring.sql

---
+++ not_boring.sql
@@ -0,0 +1,33 @@
+# Write your MySQL query statement below
+/*
+A new movie theatre has opened and people want to go see movies. THe movie theatre also
+gives out a poster that shows the ratings and descriptions
+
+Write an sql euery to output movies with an odd numbered id and a description that is not 
+'boring'. Order the result by rating
+
+For example, table cinema:
+
++---------+-----------+--------------+-----------+
+|   id    | movie     |  description |  rating   |
++---------+-----------+--------------+-----------+
+|   1     | War       |   great 3D   |   8.9     |
+|   2     | Science   |   fiction    |   8.5     |
+|   3     | irish     |   boring     |   6.2     |
+|   4     | Ice song  |   Fantacy    |   8.6     |
+|   5     | House card|   Interesting|   9.1     |
++---------+-----------+--------------+-----------+
+For the example above, the output should be:
++---------+-----------+--------------+-----------+
+|   id    | movie     |  description |  rating   |
++---------+-----------+--------------+-----------+
+|   5     | House card|   Interesting|   9.1     |
+|   1     | War       |   great 3D   |   8.9     |
++---------+-----------+--------------+-----------+
+
+*/
+
+select *
+from cinema
+where id % 2 != 0 && description not like '%boring%'
+order by rating desc;
